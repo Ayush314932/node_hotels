0adb23da7ec274c96d8c5e4edd81e3acaa0e7364
Ayush314932 <Ayushghag99@gmail.com>
2024-07-02 15:20:10 +0530

    first version of my hotels

---
+++ routes/personRoutes.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const router = express.Router();
+
+const Person=require('../models/person');
+
+
+//post route to add person
+    router.post('/', async(req,res)=>{
+        try{
+            const data= req.body//assuming the request body contains the person data
+
+        //Create a new person document using the Mongoose model
+         const newPerson= new Person(data);
+        
+        
+        //save the new person to the database
+       const savePerson = await newPerson.save()
+        console.log('data saved');
+        res.status(200).json(express.response); 
+        }catch(err){
+            console.log(err);
+            res.status(500).json({error: 'Internal server error'});
+        }
+    })
+
+    router.get('/',async (req,res)=>{
+        try{
+            const data= await Person.find();
+            console.log('data fetched ');
+            res.status(200).json(express.response);
+        }catch(err){
+            console.log(err);
+            res.status(500).json({error: 'Internal server error'});
+        }
+    });
+
+   router.get('/:workType', async (req,res)=>{
+        try{
+            const workType = req.params.workType;//  // Extract the work type form the URL parameter
+        if(workType == 'chef' || workType == 'manager' || workType == 'waiter'){
+
+            const response = await Person.find({work: workType})
+            console.log('response fetched')
+            res.status(200).json(response)
+        }else{
+                res.status(404).json({error: 'invalid syntax'})
+        }
+        }catch(err){
+            console.log(err);
+            res.status(500).json({error: 'internal server error'});
+        }
+        
+    })
+
+    // complete structure of update method;
+    router.put('/:id',async(req,res)=>{
+        try{
+            const personID = req.params.id; // extract the id from the URL parameter
+            const updatedPersonData = req.body;
+
+            const response = await Person.findByIdAndUpdate(personID,updatedPersonData,{
+                new: true ,// return the updated document
+                runValidators: true, // RUn mongoose validation
+            })
+
+            if(!response){
+                return res.status(400).json({error: 'Person not found'});
+            } 
+
+           console.log('data updated');
+           res.status(200).json(response);
+
+        }catch(err){
+            console.log(err);
+            res.status(500).json({error: 'internal server error'});
+        }
+    })
+
+    router.delete('/:id',async(res,req)=>{
+        try{
+            const personID = req.params.id;
+
+            const response = await Person.findByIdAndDelete(personID);
+            if(!response){
+                return res.status(400).json({error: 'Person not found'});
+            } 
+            console.log('data deleted');
+           res.status(200).json({message: 'peson deleted successfully'});
+        }catch(err){
+            console.log(err);
+            res.status(500).json({error: 'internal server error'});
+        }
+       
+    })
+    module.exports = router; //important for exporting router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ server.js
@@ -0,0 +1,28 @@
+    const express = require('express')
+    const app = express();
+    const db=require('./db')
+
+    const bodyParser=require('body-parser');
+    app.use(bodyParser.json());
+    
+    const cors=require('cors');
+    app.use(cors({origin:"*"}))
+
+   
+    const MenuItem = require('./models/MenuItem');
+
+    app.get('/', function (req, res){
+    res.send('Hello World welcome to our hotel ')
+    })
+
+    
+        //Import router files
+        const personRoutes = require('./routes/personRoutes');
+        const menuItemRoutes = require('./routes/menuItemRoutes')
+        //use the routers
+        app.use('/person', personRoutes);
+        app.use('/menu',menuItemRoutes);
+
+    app.listen(3000,()=>{
+        console.log('listening on port 3000')
+    })
